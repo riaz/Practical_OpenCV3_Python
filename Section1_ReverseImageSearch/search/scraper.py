36c62082962d88b026c3eb687404b8438322c272
riaz <riaz.2012@gmail.com>
2017-05-08 21:40:46 -0700

    Volume 1 Scaffolding

--- Section1_ReverseImageSearch/search/scraper.py
+++ Section1_ReverseImageSearch/search/scraper.py
@@ -1,5 +1,22 @@
 import utils
+import sys
 
-utils.sayHello()
+
+class Scraper():
+    def __init__(self):
+        if not utils.is_python3():
+            print("Please run using Python 3")
+            sys.exit(0)
+        else:
+            try:
+                imp.find_module('eggs')
+                found = True
+            except ImportError:
+                found = False
+
+    def
+
+    def run(self):
+
 
 
--- Section1_ReverseImageSearch/search/utils.py
+++ Section1_ReverseImageSearch/search/utils.py
@@ -1,4 +1,9 @@
 import sys
 
 
+def is_python3():
+    if sys.version_info >= (3, 0):
+        return True
+    return False
+
 
