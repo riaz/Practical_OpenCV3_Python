f48773197680f1070db76b504fe429f58a97dfcb
riaz <rmunshi@yahoo-inc.com>
2017-05-27 20:30:28 -0700

    Notebook and Resources

--- Section2_NumberPlateRecognition/drone/preprocess.py
+++ Section2_NumberPlateRecognition/drone/preprocess.py
@@ -3,16 +3,42 @@ import glob
 import math
 import numpy as np
 
-img = cv2.imread("../resources/images/car2.jpg")
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+image = cv2.imread("../resources/images/plate.jpg")
+image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
-#img = cv2.Canny(img, 100, 200)
+img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#kernel = np.ones((3,3),np.uint8)
-img = cv2.GaussianBlur(img,(5,5),0)
+# img = cv2.Canny(img, 100, 200)
 
-img = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-cv2.imshow("Hello", img)
+# kernel = np.ones((3,3),np.uint8)
+
+# applying gaussain blur
+img = cv2.GaussianBlur(img, (5, 5), 0)
+
+# since the number plate has numbers and the plate is aligned horizontally, we can sobel filter in vertical direction
+img = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=5)
+
+# thresholding
+_, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+morph = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+cv2.morphologyEx(img, cv2.MORPH_CLOSE, morph, img)
+
+img = cv2.GaussianBlur(img, (5, 5), 0)
+
+_, contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print(len(contours))
+
+cv2.create
+
+for cnt in contours:
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    image = cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
+
+cv2.imshow("Hello", image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
